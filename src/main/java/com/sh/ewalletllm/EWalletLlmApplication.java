91673719821acdd7df26658d673c3e88e1fd4a3b
dhgudehd98 <97084827+dhgudehd98@users.noreply.github.com>
2026-02-03 17:11:14 +0900

    first commit

---
+++ src/main/java/com/sh/ewalletllm/EWalletLlmApplication.java
@@ -0,0 +1,13 @@
+package com.sh.ewalletllm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EWalletLlmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EWalletLlmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sh/ewalletllm/EWalletLlmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sh.ewalletllm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EWalletLlmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
